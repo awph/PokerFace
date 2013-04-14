2cc80beb86e04e15efb7f324f0395c462f890a67
Diego999 <diegoantognini@gmail.com>
2013-04-14 15:40:09 +0000

    Add method getKey

--- PokerFace/PokerFace/src/ch/hearc/pokerface/gameengine/subsets/Pocket.java
+++ PokerFace/PokerFace/src/ch/hearc/pokerface/gameengine/subsets/Pocket.java
@@ -2,6 +2,7 @@
 package ch.hearc.pokerface.gameengine.subsets;
 
 import ch.hearc.pokerface.gameengine.cards.Card;
+import ch.hearc.pokerface.gameengine.cards.CardColor;
 
 
 public class Pocket extends CardSubset
@@ -18,6 +19,26 @@ public class Pocket extends CardSubset
 	/*------------------------------------------------------------------*\
 	|*							Methodes Public							*|
 	\*------------------------------------------------------------------*/
+
+	public String getKey()
+	{
+		StringBuilder stringBuilder = new StringBuilder("");
+
+		CardColor color = this.iterator().next().getColor();
+		boolean sameColor = true;
+		for(Card c:this)
+		{
+			if (c.getColor() != color)
+			{
+				sameColor = false;
+			}
+			stringBuilder.append(c.getValue().getStringValue());
+		}
+
+		stringBuilder.append((sameColor) ? "s" : "o");
+
+		return stringBuilder.toString();
+	}
 
 	@Override
 	public void add(Card c)
