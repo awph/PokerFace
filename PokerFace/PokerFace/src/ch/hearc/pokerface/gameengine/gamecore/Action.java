14e4ddced0b195fd8fd97baecb27b1d9784e1133
thedarkmammouth <alexandre.perez92@gmail.com>
2013-05-03 07:17:46 +0000

    Add Action Enum

---
+++ PokerFace/PokerFace/src/ch/hearc/pokerface/gameengine/gamecore/Action.java
@@ -0,0 +1,32 @@
+
+package ch.hearc.pokerface.gameengine.gamecore;
+
+public enum Action
+{
+	Bet("bets"), Raise("raises"), Fold("sits out"), Call("calls"), Check("checks"), Allin("all in with"), PostSmallBlind("posts small blind"), PostBigBlind("posts big blind"), Winner("Winner"), Loser("Loser");
+
+	/*------------------------------------------------------------------*\
+	|*							Attributs Private						*|
+	\*------------------------------------------------------------------*/
+
+	private String	valueString;
+
+	/*------------------------------------------------------------------*\
+	|*							Constructeurs							*|
+	\*------------------------------------------------------------------*/
+
+	private Action(String valString)
+	{
+		valueString = valString;
+	}
+
+	/*------------------------------------------------------------------*\
+	|*							Methodes Public							*|
+	\*------------------------------------------------------------------*/
+
+	@Override
+	public String toString()
+	{
+		return valueString;
+	}
+}
