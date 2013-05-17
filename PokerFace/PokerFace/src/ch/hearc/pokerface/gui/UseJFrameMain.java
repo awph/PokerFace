b7aff8d63ca7357176619e5850b28cb6f59e90ba
Diego999 <diegoantognini@gmail.com>
2013-05-17 07:21:27 +0000

    Uncomment the splashScreenLoading

--- PokerFace/PokerFace/src/ch/hearc/pokerface/gui/UseJFrameMain.java
+++ PokerFace/PokerFace/src/ch/hearc/pokerface/gui/UseJFrameMain.java
@@ -1,6 +1,10 @@
 
 package ch.hearc.pokerface.gui;
 
+import java.io.IOException;
+
+import javax.swing.UIManager;
+
 import ch.hearc.pokerface.gameengine.cards.Card;
 import ch.hearc.pokerface.gameengine.cards.CardColor;
 import ch.hearc.pokerface.gameengine.cards.CardValue;
@@ -9,45 +13,40 @@ import ch.hearc.pokerface.gameengine.gamecore.SoundEngine;
 import ch.hearc.pokerface.gameengine.statistics.Statistics;
 import ch.hearc.pokerface.gameengine.subsets.Pocket;
 
-import javax.swing.*;
-import java.io.IOException;
-
 public class UseJFrameMain
 {
 	/*------------------------------------------------------------------*\
 	|*							Methodes Public							*|
 	\*------------------------------------------------------------------*/
-	
+
 	public static void main(String[] args) throws IOException
 	{
 		main();
 		setupLookAndFeel();
 	}
-	
+
 	public static void main()
 	{
-		//splashScreenLoading();
-		
+		splashScreenLoading();
+
 		new JFrameMain();
 	}
-	
+
 	/*------------------------------------------------------------------*\
 	|*							Methodes Private						*|
 	\*------------------------------------------------------------------*/
-	
+
 	private static void splashScreenLoading()
 	{
 		Pocket p = new Pocket();
 		p.add(new Card(CardValue.Ace, CardColor.Spades));
 		p.add(new Card(CardValue.Ace, CardColor.Diamonds));
-		
-		//Il faut précharger le XML !
-		//TODO : Le faire pendant le splashscreen
+
 		Statistics.getPreFlopValues(p, 2);
 		SoundEngine.getInstance();
 		HandsPokerMap.getInstance();
 	}
-	
+
 	private static void setupLookAndFeel()
 	{
 		try
