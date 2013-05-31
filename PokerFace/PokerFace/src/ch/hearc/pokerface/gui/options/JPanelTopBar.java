921f7730b4d3add5895c004d94e5d26d4b58de67
Diego999 <diegoantognini@gmail.com>
2013-05-31 21:47:54 +0000

    Put the poker rules in static

--- PokerFace/PokerFace/src/ch/hearc/pokerface/gui/options/JPanelTopBar.java
+++ PokerFace/PokerFace/src/ch/hearc/pokerface/gui/options/JPanelTopBar.java
@@ -52,6 +52,35 @@ public final class JPanelTopBar extends JPanel
 
 	private JFrameMain						frameMain;
 
+	private static final String				RULES		= "<html><h3>The Cards</h3>"
+																+ "<p>At the beginning, each player receive 2 cards that the other(s) player(s) can't see. In the deck, there are in total 52 cards.<br />"
+																+ "During the game, others cards will appear at the middle : first 3, then 1 and finally 1 other. These cards are shared with the other players<br />"
+																+ "The purpose is to have the best hand ranking. To help you, there is at the bottom left all the handrankings possible</p>"
+																+ "<h3>Game process</h3>"
+																+ "<p>In the poker texas holdem, there are 4 main states and few bettings states. In the oreders, the state are :"
+																+ "<ul>"
+																+ "<li><b>Pre flop</b> : You receive your 2 cards</li>"
+																+ "<li><b>Flop</b> : 3 cards appear over the board</li>"
+																+ "<li><b>Turn</b> : 1 card appears over the board</li>"
+																+ "<li><b>River</b> : 1 last card appears over the board</li>"
+																+ "</ul>"
+																+ "Between each step there is a betting state. It will be described afterwards.<br />"
+																+ "After the last betting state, all the players in play show their cards. If a player has less good cards than another one, he has the possibility to not show his cards.<br />"
+																+ "At the end, the best player take all the money. If there is more than one winner (tie), the pot is divided up.</p>"
+																+ "<h3>Bet</h3>"
+																+ "During a game, each player has a role : "
+																+ "<ul>"
+																+ "<li>Small Blind (only when there are more than 2 players)</li>"
+																+ "<li>Big Blind</li>"
+																+ "<li>Dealer</li>"
+																+ "<li>Nothing</li>"
+																+ "</ul>"
+																+ "During the first betting state, after the preflop state, the big blind must bet an amount (defined by the table options) and the big blind twice more.<br />"
+																+ "The dealer deals the card then the small blind bets and finally the big blind. Afterwards, the next player could play and make a decision.<br /> After all players have played, the small blind has to play and the Big Blind has to finish the turn if no body has raised.<br />"
+																+ "After the preflop, there is no more roles.<br />" + "The diffections action possible are :" + "<ul>" + "<li><b>Fold</b> : You left the current game</li>"
+																+ "<li><b>Check</b> : If nobody has already bet, that mean \"I continue but I bet nothing\"</li>" + "<li><b>Bet </b> : Same than check but you define the bet</li>"
+																+ "<li><b>Call</b> : To continue the game, you have to pay the bet</li>" + "<li><b>Raise</b> : If you have good hands or want to bluff, you could pay more than necessary</li>" + "</ul></html>";
+
 	/*------------------------------------------------------------------*\
 	|*							Constructeurs							*|
 	\*------------------------------------------------------------------*/
@@ -206,42 +235,7 @@ public final class JPanelTopBar extends JPanel
 
 	private void showHelp()
 	{
-		String rules =
-				"<html><h3>The Cards</h3>" +
-				"<p>At the beginning, each player receive 2 cards that the other(s) player(s) can't see. In the deck, there are in total 52 cards.<br />" +
-				"During the game, others cards will appear at the middle : first 3, then 1 and finally 1 other. These cards are shared with the other players<br />" +
-				"The purpose is to have the best hand ranking. To help you, there is at the bottom left all the handrankings possible</p>" +
-				"<h3>Game process</h3>" +
-				"<p>In the poker texas holdem, there are 4 main states and few bettings states. In the oreders, the state are :" +
-				"<ul>" +
-				"<ol><b>Pre flop</b> : You receive your 2 cards</ol>" +
-				"<ol><b>Flop</b> : 3 cards appear over the board</ol>" +
-				"<ol><b>Turn</b> : 1 card appears over the board</ol>" +
-				"<ol><b>River</b> : 1 last card appears over the board</ol>" +
-				"</ul>" +
-				"Between each step there is a betting state. It will be described afterwards.<br />" +
-				"After the last betting state, all the players in play show their cards. If a player has less good cards than another one, he has the possibility to not show his cards.<br />" +
-				"At the end, the best player take all the money. If there is more than one winner (tie), the pot is divided up.</p>" +
-				"<h3>Bet</h3>" +
-				"During a game, each player has a role : " +
-				"<ul>" +
-				"<li>Small Blind (only when there are more than 2 players)</li>" +
-				"<li>Big Blind</li>" +
-				"<li>Dealer</li>" +
-				"<li>Nothing</li>" +
-				"</ul>" +
-				"During the first betting state, after the preflop state, the big blind must bet an amount (defined by the table options) and the big blind twice more.<br />" +
-				"The dealer deals the card then the small blind bets and finally the big blind. Afterwards, the next player could play and make a decision.<br /> After all players have played, the small blind has to play and the Big Blind has to finish the turn if no body has raised.<br />" +
-				"After the preflop, there is no more roles.<br />" +
-				"The diffections action possible are :" +
-				"<ul>" +
-				"<li><b>Fold</b> : You left the current game</li>" +
-				"<li><b>Check</b> : If nobody has already bet, that mean \"I continue but I bet nothing\"</li>" +
-				"<li><b>Bet </b> : Same than check but you define the bet</li>" +
-				"<li><b>Call</b> : To continue the game, you have to pay the bet</li>" +
-				"<li><b>Raise</b> : If you have good hands or want to bluff, you could pay more than necessary</li>" +
-				"</ul></html>";
-		JOptionPane.showMessageDialog(this, rules, "Poker Rules, Texas Hold'Em No Limit", JOptionPane.INFORMATION_MESSAGE, ImageShop.ICON_BUTTON_HELP);
+		JOptionPane.showMessageDialog(this, RULES, "Poker Rules, Texas Hold'Em No Limit", JOptionPane.INFORMATION_MESSAGE, ImageShop.ICON_BUTTON_HELP);
 	}
 
 	private ActionListener	updateClockAction	= new ActionListener()
