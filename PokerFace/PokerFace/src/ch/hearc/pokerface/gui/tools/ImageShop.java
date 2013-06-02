98704f85114ce30733556c70dee681de28c1630d
thedarkmammouth <alexandre.perez92@gmail.com>
2013-06-02 13:29:56 +0000

    replace HE-Arc logo with transparent

--- PokerFace/PokerFace/src/ch/hearc/pokerface/gui/tools/ImageShop.java
+++ PokerFace/PokerFace/src/ch/hearc/pokerface/gui/tools/ImageShop.java
@@ -46,10 +46,11 @@ public class ImageShop
 	private static final String		NAME_IMAGE_REMOVEPROFILE			= "remove.png";
 	private static final String		NAME_ICON_LOGO_TOPBAR				= "logo_pokerface_topbar.png";
 
-	private static final String		NAME_IMAGE_HE_ARC					= "hearc.jpg";
+	private static final String		NAME_IMAGE_HE_ARC					= "hearc.png";
+
 	private static final boolean	IS_BLOQUANT							= true;
 
-	public static final ImageIcon	IMAGE_HE_ARC						= ImageTools.loadScaledIcon(PATH + NAME_IMAGE_HE_ARC, 0.4, IS_BLOQUANT);
+	public static final ImageIcon	IMAGE_HE_ARC						= ImageTools.loadIconJar(PATH + NAME_IMAGE_HE_ARC, IS_BLOQUANT);
 
 	public static final ImageIcon	ICON_BUTTON_QUIT_GAME				= ImageTools.loadIconJar(PATH + NAME_IMAGE_BUTTON_QUIT_GAME, IS_BLOQUANT);
 	public static final ImageIcon	ICON_BUTTON_QUIT_APPLICATION		= ImageTools.loadIconJar(PATH + NAME_IMAGE_BUTTON_QUIT_APPLICATION, IS_BLOQUANT);
